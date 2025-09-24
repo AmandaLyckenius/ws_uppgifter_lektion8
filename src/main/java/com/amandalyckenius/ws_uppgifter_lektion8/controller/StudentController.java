b865eac640049b0d497f01fd9628deab584e4f5a
amandalyckenius <alyckenius@gmail.com>
2025-09-24 14:53:17 +0200

    add: student controller and /create

---
+++ src/main/java/com/amandalyckenius/ws_uppgifter_lektion8/controller/StudentController.java
@@ -0,0 +1,34 @@
+package com.amandalyckenius.ws_uppgifter_lektion8.controller;
+
+
+import com.amandalyckenius.ws_uppgifter_lektion8.model.Student;
+import com.amandalyckenius.ws_uppgifter_lektion8.repository.StudentRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepository repo;
+
+    public StudentController(StudentRepository repo) {
+        this.repo = repo;
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> create(@RequestBody Student student) {
+        student.setId(null);
+        Student saved = repo.save(student);
+        return ResponseEntity
+                .created(URI.create("/students/" + saved.getId()))
+                .body(saved);
+    }
+
+
+}
